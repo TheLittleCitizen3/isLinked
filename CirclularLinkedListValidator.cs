400b6955d170ace92e94c1cf47fab7eed6eb636f
thelittlecitizen3 <thelittlecitizen3@gmail.com>
2020-08-11 18:17:07 +0300

    Created validation fo linked list

---
+++ CirclularLinkedListValidator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class CirclularLinkedListValidator 
+    {
+        public bool IsCircular<T>(LinkedList<T> linkedList)
+        {
+            if (linkedList == null)
+            {
+                return false;
+            };
+
+            LinkedListNode<T> listFirstIndexNode = linkedList.First;
+
+            if (listFirstIndexNode.Next == null)
+            {
+                return false;
+            }
+            LinkedListNode<T> listSecondIndexNode = listFirstIndexNode.Next;
+  
+            while (listSecondIndexNode.Next != null)
+            {
+                if (listSecondIndexNode.Next == null)
+                {
+                    return false;
+                }
+                listSecondIndexNode = listSecondIndexNode.Next;
+                if (listFirstIndexNode == listSecondIndexNode)
+                {
+                    return true;
+                }
+                if (listSecondIndexNode.Next == null)
+                {
+                    return false;
+                }
+                listSecondIndexNode = listSecondIndexNode.Next;
+                listFirstIndexNode = listFirstIndexNode.Next;
+
+            }
+            return false;
+        }
+
+    }
+
+
+}
